861ce97ef402d2d668ad4a965b678092ad83bfe9
JosiahFu <josiahfu@gmail.com>
2022-12-13 20:39:46 -0800

    Initial commit: Preprocessor

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+fs.readFile('./in.txt', 'utf8', (err: any, data: string): void => {
+    let points = data
+        .split('\n') // Split by lines
+        .filter(e => e[0] != '#') // Remove commented
+        .map(e => e.split(' ')) // Split by spaces
+        .filter(e => e.length > 2); // Remove any lines with one item
+
+    let pointsList = points.flat(1);
+
+    let pointIndices = points
+        .map(e => e.length) // Convert to lengths
+        .map((value, i, points) => points.slice(0, i + 1).reduce((prev, cur) => prev + cur))
+    pointIndices.unshift(0);
+
+    console.log('P_{s}=\\left[' + pointsList.join(',') + '\\right]');
+    console.log('P_{is}=\\left[' + pointIndices.join(',') + '\\right]');
+});
